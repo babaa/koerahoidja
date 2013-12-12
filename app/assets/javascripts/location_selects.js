796449bb329fab23853e3f50c996da2510d8f6d6
Jette Feigenbaum <jettefeigenbaum@gmail.com>
2013-12-13 00:41:08 +0200

    location selects javascript file

---
+++ app/assets/javascripts/location_selects.js
@@ -0,0 +1,18 @@
+$('.county-select').on('change', function() {
+  $.get('/locations/parishes_and_towns', {name: $('option:selected', this).text()}, function(data) {
+    $('.parish-select').empty();
+    populateSelect($('.parish-select'), data);
+    $('.parish-select').change();
+  });
+});
+
+function populateSelect(obj, data) {
+  for(var i in data) {
+
+    var alreadyExists = false;
+    $('option:selected', obj).each(function(_, v) {
+      if($(v).val() == data[i][1]) {
+        alreadyExists = true;
+        return false;
+      }
+    });
